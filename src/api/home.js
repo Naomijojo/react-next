2579b5ef2399026d7fc689fbb5abdc9afb2bb7c0
JOJO <ch420ou@gmail.com>
2025-07-24 19:49:43 +0800

    fix: mock data

--- src/api/home.js
+++ src/api/home.js
@@ -2,23 +2,15 @@ import axios from "axios"
 import recommendData from '../mock/data/recommend.json'
 import articleData from '../mock/data/article.json'
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? '' : ''
-
-// homeApi 中直接導入 JSON 文件,開發環境直接返回 mock 數據
+// homeApi 中直接導入 JSON 文件,直接返回 mock 數據
 
 export const homeApi = { 
   getRecommend: async() => { 
-    if (process.env.NODE_ENV === 'development') {
-      return { code: 200, data: recommendData }
-    }
-    const { data } = await axios.get('/mock/recommend')
-    return data
+    // 暫時在開發和生產環境都使用 mock 數據
+    return { code: 200, data: recommendData }
   },
   getArticle: async() =>{
-    if (process.env.NODE_ENV === 'development') {
-      return { code: 200, data: articleData }
-    }
-    const { data } = await axios.get('/mock/article')
-    return data
+    // 暫時在開發和生產環境都使用 mock 數據
+    return { code: 200, data: articleData }
   }
 }
